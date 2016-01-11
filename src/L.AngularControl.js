cc98befd7351d2ba68af12162cfb5eb3c689e574
Grant Harris <grant@eatart.org>
2016-01-11 13:16:16 -0800

    On second thought we don't really need the anoymous function

--- src/L.AngularControl.js
+++ src/L.AngularControl.js
@@ -1,51 +1,50 @@
  'use strict';
- (function () {
-     L.AngularControl = L.Control.extend({
-        options: {
-             position: 'bottomleft',
-             template: ''
-         },
-         onAdd: function(map) {
-             var that = this;
-             var container = L.DomUtil.create('div', 'angular-control-leaflet');
-             angular.element(document).ready(function() {
-                 // Grab the injector for the current angular app
-                 var $injector = angular.element(document.querySelector('[ng-app]')).injector();
+ 
+ L.AngularControl = L.Control.extend({
+    options: {
+         position: 'bottomleft',
+         template: ''
+     },
+     onAdd: function(map) {
+         var that = this;
+         var container = L.DomUtil.create('div', 'angular-control-leaflet');
+         angular.element(document).ready(function() {
+             // Grab the injector for the current angular app
+             var $injector = angular.element(document.querySelector('[ng-app]')).injector();
 
-                 var $rootScope = $injector.get('$rootScope'),
-                     $compile = $injector.get('$compile'),
-                     $controller = $injector.get('$controller');
+             var $rootScope = $injector.get('$rootScope'),
+                 $compile = $injector.get('$compile'),
+                 $controller = $injector.get('$controller');
 
-                 var scope = $rootScope.$new(true);
+             var scope = $rootScope.$new(true);
 
-                 var element = angular.element(container);
-                 element.html(that.options.template);
+             var element = angular.element(container);
+             element.html(that.options.template);
 
-                 var link = $compile(element);
+             var link = $compile(element);
 
-                 if (that.options.controller) {
-                     var controller = $controller(that.options.controller, {
-                         '$map': map,
-                         '$scope': scope,
-                         '$element': element
-                     });
+             if (that.options.controller) {
+                 var controller = $controller(that.options.controller, {
+                     '$map': map,
+                     '$scope': scope,
+                     '$element': element
+                 });
 
-                     if (that.options.controllerAs) {
-                         scope[that.options.controllerAs] = controller;
-                     }
-
-                     element.data('$ngControllerController', controller);
-                     element.children().data('$ngControllerController', controller);
+                 if (that.options.controllerAs) {
+                     scope[that.options.controllerAs] = controller;
                  }
 
-                 link(scope);
-                 scope.$apply();
-             });
-             return container;
-         }
-     });
+                 element.data('$ngControllerController', controller);
+                 element.children().data('$ngControllerController', controller);
+             }
 
-     L.angularControl = function(options) {
-         return new L.AngularControl(options);
-     };
- })();
+             link(scope);
+             scope.$apply();
+         });
+         return container;
+     }
+ });
+
+ L.angularControl = function(options) {
+     return new L.AngularControl(options);
+ };
